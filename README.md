c1d7689bb26b7d0e2e1c3cef16c705f85d88b680
Nikola Plesa <nickplesha@gmail.com>
2017-07-17 20:05:02 +0200

    Improve performance and update style

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 Mapillary Chrome extension
 ===================
 
-Browser extension for [Chrome](https://www.google.com/chrome/) that shows a random beautiful image from [Mapillary](https://www.mapillary.com) every time you open a new tab. Uses [MapillaryJS](https://github.com/mapillary/mapillary-js) to allow you to explore the images further. Download it on the [Chrome web store](https://chrome.google.com/webstore/detail/mapillary-%E2%80%93-crowdsourced/lkokbfpnhgcnkhecedjgdemlohbaomjd)
+Browser extension for [Chrome](https://www.google.com/chrome/) that shows a random beautiful image from [Mapillary](https://www.mapillary.com) every time you open a new tab. Download it on the [Chrome web store](https://chrome.google.com/webstore/detail/mapillary-%E2%80%93-crowdsourced/lkokbfpnhgcnkhecedjgdemlohbaomjd)
 
 ## Install
 
@@ -12,8 +12,6 @@ Clone this repo and then run `npm install`
 `npm start` will start a local server and open a new tab with the page.
 
 Alternatively, you can navigate to `chrome://extensions` in Chrome and drag-and-drop the whole folder there. That way you'll install the extension and it will also update automatically as you change the code.
-
-You'll need a `clientId` from Mapillary to be able to use the [MapillaryJS](https://github.com/mapillary/mapillary-js) viewer, you can create one on [your Mapillary profile page](http://www.mapillary.com/map/settings/integrations).
 
 ## Contributing
 
--- js/app.js
+++ js/app.js
@@ -2,17 +2,32 @@
     'use strict';
     
     function RandomMapillary(locations) {
-        var clientId = '<your client id here>',
-            mapillaryUrl = 'https://www.mapillary.com/map/im/',
-            mapillaryImageUrl = 'https://d1cuyjsrcm0gby.cloudfront.net/';
+        var mapillaryUrl = 'https://www.mapillary.com/app/?pKey=',
+            mapillaryImageUrl = 'https://d1cuyjsrcm0gby.cloudfront.net/',
+            mapillaryMapUrl = 'http://mapillary-staticmaps.mapillary.io/v3/staticmap/image/180/180/',
+            nextImageKey = 'nextImage',
+            nextDataUrlKey = 'nextDataUrl';
         
         this.getRandomLocation = function() {
+            var nextImage = localStorage.getItem(nextImageKey);
+            if (nextImage) {
+                localStorage.removeItem(nextImageKey);
+                var image = JSON.parse(nextImage)
+                image.isPreloaded = true;
+                return image;
+            }
             return locations[Math.floor(Math.random() * locations.length)];            
         }
         
-        this.setBackgroundImage = function(sequence) {
+        this.setBackgroundImage = function(image) {
             var backgroundImage = new Image(),
-                imageUrl = mapillaryImageUrl + sequence + '/thumb-2048.jpg';
+                imageUrl = mapillaryImageUrl + image.key + '/thumb-2048.jpg';
+            
+             if (image.isPreloaded) {
+                return new Promise(function(resolve) {
+                    resolve(localStorage.getItem(nextDataUrlKey));
+                })
+            } 
                 
             return new Promise(function(resolve, reject) {
                 backgroundImage.onload = function() {
@@ -24,61 +39,72 @@
                 backgroundImage.src = imageUrl
             })
         }
-        
-        this.showMap = function(location) {
-            var map = L.map('map', {zoomControl: false}),
-                osmUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',              
-                osmAttrib = '© <a href="https://openstreetmap.org">OpenStreetMap</a> contributors',
-                osm = new L.TileLayer(osmUrl, { maxZoom: 18, attribution: osmAttrib}),
-                marker = L.marker({
-                    lat: location.lat,
-                    lon: location.lon
-                });
+
+        this.setMapImage = function(key) {
+            var backgroundImage = new Image(),
+                imageUrl = mapillaryMapUrl + key + '.png?zoom=3&style=mapbox_streets';
                 
-            map.addLayer(osm);
-            map.setView([location.lat, location.lon], 1);
-            marker.addTo(map);
-            
-            document.getElementById('map').href = mapillaryUrl + location.key + '/map';
+            return new Promise(function(resolve, reject) {
+                backgroundImage.onload = function() {
+                    resolve(imageUrl);
+                }
+                backgroundImage.onerror = function() {
+                    reject();
+                }
+                backgroundImage.src = imageUrl
+            })
         }
-        
-        this.loadMapillaryJS = function() {
-            var script = document.createElement("script");
-            script.src = "/node_modules/mapillary-js/dist/mapillary-js.min.js";
-            script.type = "text/javascript";
-            document.head.appendChild(script);
-        }
-       
-        this.setMapillaryView = function(sequence) {
-            var viewer = new Mapillary.Viewer('mly',
-            clientId,
-            sequence,
-            {
-                cover: false,
-                renderMode: Mapillary.RenderMode.Fill
-            });
-            window.addEventListener("resize", function() { viewer.resize(); });
-        }
+
+        this.preloadNextImage = function(image) {
+            var imageUrl = mapillaryImageUrl + image.key + '/thumb-2048.jpg';
+
+            var xmlHTTP = new XMLHttpRequest();
+            xmlHTTP.open("GET", imageUrl, true);
+            xmlHTTP.responseType = "blob";
+            xmlHTTP.timeout = 15000;
+
+            xmlHTTP.onload = function() {
+                if (xmlHTTP.status !== 200) {
+                    return;
+                }
+                localStorage.removeItem(nextImageKey);
+                localStorage.removeItem(nextDataUrlKey);
+                var reader = new FileReader();
+                reader.onloadend = function() {
+                    localStorage.setItem(nextImageKey, JSON.stringify(image));
+                    localStorage.setItem(nextDataUrlKey, reader.result);
+                }
+                reader.readAsDataURL(xmlHTTP.response);
+            };
+
+            xmlHTTP.send(null); 
+        } 
         
         this.init = function() {
             var self = this,
                 location = this.getRandomLocation(),
                 background = document.getElementById('background'),
                 exploreButton = document.getElementById('explore'),
+                map = document.getElementById('map'),
+                osm = document.getElementById('osm'),
                 shareMenu = new ShareMenu(mapillaryUrl + location.key);
                 
-            this.setBackgroundImage(location.key).then(function(imageUrl) {
+            this.setBackgroundImage(location).then(function(imageUrl) {
                 background.style.backgroundImage = "url('" + imageUrl + "')";
                 background.style.opacity = "1";
-                self.showMap(location);
-                self.loadMapillaryJS();
+                self.preloadNextImage(self.getRandomLocation());
             })
-            
-            exploreButton.onclick = function() {
-                self.setMapillaryView(location.key);
-                background.style.opacity = 0;
-                setTimeout(function() {background.style.display = 'none';}, 300) 
-            };
+
+            this.setMapImage(location.key).then(function(imageUrl) {
+                map.style.backgroundImage = "url('" + imageUrl + "')";
+                map.style.opacity = "0.9";   
+                map.href = mapillaryUrl + location.key + '&focus=map';
+                osm.style.opacity = "1";
+                osm.href = 'https://www.openstreetmap.org/#map=15/' + location.lat + '/' + location.lon;
+            })
+
+            exploreButton.href = mapillaryUrl + location.key + '&focus=photo';
+
         }
         
         this.init();
@@ -101,7 +127,7 @@
                 shareMenu = document.getElementById('share-menu');
                         
             options[0].href = shareUrls.facebook + url;
-            options[1].href = shareUrls.twitter + defaultTwitterText + url;
+            options[1].href = shareUrls.twitter + defaultTwitterText + '&url=' + url;
             options[2].href = shareUrls.googlePlus + url;
             options[3].href = shareUrls.tumblr + url;
             options[4].onclick = function() {
@@ -124,6 +150,13 @@
         .then(function(response) {
             return response.json();
         }).then(function(data) {
+
+            var keys = [];
+
+            for (var index = 0; index < data.savedLocations.length; index++) {
+                keys.push(data.savedLocations[index].key);   
+            }
+            localStorage.setItem('keys', JSON.stringify(keys));
             var randomMapillary = new RandomMapillary(data.savedLocations);
         });
 
